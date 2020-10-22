98aca507e57c4412f56c5a6840596d78a63426ec
Jamie Hunter <hello@jamiehunter.design>
2020-10-22 12:39:29 +0200

    Revert frontmatter

--- src/parser.js
+++ src/parser.js
@@ -47,8 +47,8 @@ function collectPosts(data, config) {
 				imageUrls: []
 			},
 			frontmatter: {
-				Title: getPostTitle(post),
-				Date: getPostDate(post)
+				title: getPostTitle(post),
+				date: getPostDate(post)
 			},
 			content: translator.getPostContent(post, turndownService, config)
 		}));
